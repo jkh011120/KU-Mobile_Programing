c2e8b167fef37b643de0c3153b8edddc92703893
jkh011120 <jkh01120@gmail.com>
2025-04-16 12:57:31 +0900

    add module eweek07a

---
+++ eweek07a/src/main/java/com/example/eweek07a/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.eweek07a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.eweek07a.example01.uicomponents.MainScreen
+import com.example.eweek07a.ui.theme.MyLecETheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyLecETheme {
+                MainScreen()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyLecETheme {
+        Greeting("Android")
+    }
+}
---
+++ eweek07a/src/main/java/com/example/eweek07a/example01/navGraph/NavGraph.kt
@@ -0,0 +1,49 @@
+package com.example.eweek07a.example01.navGraph
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.eweek07a.example01.model.Routes
+import com.example.eweek07a.example01.uicomponents.HomeScreen
+import com.example.eweek07a.example01.uicomponents.Screen_A
+import com.example.eweek07a.example01.uicomponents.Screen_B
+import com.example.eweek07a.example01.uicomponents.Screen_C
+import com.example.eweek07a.example01.uicomponents.Screen_D
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = Routes.Home.route) {
+
+        composable(route = Routes.Home.route) {
+            HomeScreen(
+                onNavigateA = { navController.navigate(Routes.ScreenA.route) },
+                onNavigateB = { navController.navigate(Routes.ScreenB.route) }
+            )
+        }
+
+        composable(route = Routes.ScreenA.route) {
+            Screen_A(
+                onNavigateC = { navController.navigate(Routes.ScreenC.route) },
+                onNavigateD = { navController.navigate(Routes.ScreenD.route) })
+        }
+
+        composable(route = Routes.ScreenB.route) {
+            Screen_B()
+        }
+
+        composable(route = Routes.ScreenC.route) {
+            Screen_C(onNavigate = { navController.navigate(Routes.Home.route) })
+        }
+
+        composable(route = Routes.ScreenD.route) {
+            Screen_D()
+        }
+
+
+    }
+}
